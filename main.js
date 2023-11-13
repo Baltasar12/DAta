017e654e133a20201f7456bd227917358cbdf87f
Baltasar12 <baltasarbarbaglia@gmail.com>
2023-11-13 09:48:40 -0300

    Commit 1

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow, screen } = require('electron');
+let mainWindow;
+
+app.whenReady().then(() => {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  mainWindow = new BrowserWindow({
+    width: width,
+    height: height,
+    maxWidth: width,
+    maxHeight: height,
+  });
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
